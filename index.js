4bc8b92ebd3dd502d771ef48e11208f86f271105
Hape Cook-Porter & Oliver Stigley <pair+hapekituarangi.ostigley@devbootcamp.com>
2016-05-23 10:32:38 +1200

    Updates

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var example = require('./views/example.hbs')
 xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, data) {
   if (err) console.log(err) // do something
 
-  console.log(data.body)
   data = JSON.parse(data.body)
-  document.body.innerHTML = example({ name: data.velocity });
+  console.log(data)
+  document.body.innerHTML = example({ name: data.name });
 })
 
